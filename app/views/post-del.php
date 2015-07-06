bcc6edca04b68f83f997fa43340752a642bafa9b
unknown <evilnapsis@gmail.com>
2015-07-05 22:58:37 -0700

    Cleaning Directory

--- app/views/post-del.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include "app/core/conection.php";
-
-$sql="DELETE FROM post WHERE id=$_GET[id] ";
-$query=$con->query($sql);
-echo "<script>window.location=\"index.php?view=post-list\";</script>";?>
